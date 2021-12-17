11da8c5df99a53b5fec5f15a7f7128c6935a125a
Erin Knapp <erin.knapp@gmail.com>
2021-12-17 18:24:30 -0500

    adding in require and gitignore file

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
 
-inquirer
-  .prompt([
+const promptUser = () => {
+  return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
       message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your Github Username:'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
     }
-  ])
-  .then(answers => console.log(answers));
+  ]);
+};
 
+promptUser().then(answers => console.log(answers));
 
+const promptProject = () => {
+  console.log(`
+  =====================
+  Add a New Project
+  =====================
+  `)
+}
 
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
